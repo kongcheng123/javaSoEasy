922d22da6aa4cca0fb1433e81ff7c1ae87ebd34f
kongcheng123 <1175642794@qq.com>
2018-03-29 00:03:19 +0800

    ConcurrentHashMap 添加

---
+++ src/Concurrent/concurrenthashmap_t.java
@@ -0,0 +1,20 @@
+package Concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author xy
+ * @date 2018/03/29 00:01
+ */
+public class concurrenthashmap_t {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void test(){
+        ConcurrentHashMap<String,String> chm = new ConcurrentHashMap<String,String>();
+    }
+}
